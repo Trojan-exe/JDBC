09571c4b3bce7b2cced98529c10be0b0fd39c8aa
Saurabh Sunil Kumatkar <saurabhkumatkar2000@gmail.com>
2023-02-07 21:50:28 +0530

    commit

--- JDBC-CallableStatement-Applications/src/com/sk/CallableStatementBasicApplications/GetAverageFunction.java
+++ JDBC-CallableStatement-Applications/src/com/sk/CallableStatementBasicApplications/GetAverageFunction.java
@@ -1,8 +1,13 @@
 /* Stored Procedure
- 	create or replace procedure getSal(no IN number, sal OUT float) AS
+ 	create or replace function getAvg(no1 IN number, no2 IN number) return float 
+ 	AS
+ 	sal1 float;
+ 	sal2 float;
  	BEGIN
- 		select ESAL into sal from emp where ENO = no;
- 	END getSal;
+ 		select ESAL into sa1l from emp where ENO = no1;
+ 		select ESAL into sa12 from emp where ENO = no2;
+ 		return (sal1+sal2)/2;
+ 	END getAvg;
  	/
   
  */
@@ -16,7 +21,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Types;
 
-public class SingleValueReadProcedure {
+public class GetAverageFunction {
 
 	public static void main(String[] args) {
 		Connection con = null;
@@ -27,16 +32,20 @@ public class SingleValueReadProcedure {
 			Class.forName("oracle.jdbc.OracleDriver");
 			con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "System", "root");
 			br = new BufferedReader(new InputStreamReader(System.in));
-			cst = con.prepareCall("{call getSal(?,?)}");
+			cst = con.prepareCall("{? = call getAVG(?,?)}");
 			
-			System.out.println("Enter Employee No : ");
-			int eno = Integer.parseInt(br.readLine());
+			System.out.println("Enter First Employee No : ");
+			int feno = Integer.parseInt(br.readLine());
 			
-			cst.setInt(1, eno);
-			cst.registerOutParameter(2, Types.FLOAT);
+			System.out.println("Enter Second Employee No : ");
+			int seno = Integer.parseInt(br.readLine());
+			
+			cst.setInt(2, feno);
+			cst.setInt(3, seno);
+			cst.registerOutParameter(1, Types.FLOAT);
 			cst.execute();
 			
-			System.out.println("Employee Salary : " + cst.getFloat(2));
+			System.out.println("Average Employee Salary : " + cst.getFloat(1));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}finally {
